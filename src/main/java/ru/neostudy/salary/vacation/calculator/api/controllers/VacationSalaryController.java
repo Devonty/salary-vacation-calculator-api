ca2988e1a88977e6244206644834cf1667948182
Devonty <smurf20044@mail.ru>
2024-09-22 22:04:52 +0300

    math mistake fix

--- src/main/java/ru/neostudy/salary/vacation/calculator/api/controllers/VacationSalaryController.java
+++ src/main/java/ru/neostudy/salary/vacation/calculator/api/controllers/VacationSalaryController.java
@@ -11,34 +11,44 @@ import java.time.LocalDate;
 import java.time.temporal.ChronoUnit;
 
 @RestController
-public class SalaryVacationController {
-    public static final String CALC_VACATION_SALARY = "/api/calc_vacation_salary";
+public class VacationSalaryController {
+    public static final String GET_VACATION_SALARY = "/api/calc_vacation_salary";
 
 
-    @GetMapping(value = CALC_VACATION_SALARY, params = {"average_month_salary", "start_date", "end_date"})
-    public Double calcVacationSalary(
+    @GetMapping(value = GET_VACATION_SALARY, params = {"average_month_salary", "start_date", "end_date"})
+    public Double getVacationSalary(
             @RequestParam(value = "average_month_salary") double averageMonthSalary,
             @RequestParam(value = "start_date") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate startDate,
             @RequestParam(value = "end_date") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate endDate) {
 
-        long days = ChronoUnit.DAYS.between(startDate, endDate) + 1; // +1 includes start day
+        long dayCount = ChronoUnit.DAYS.between(startDate, endDate) + 1; // +1 includes start day
 
-        if (days <= 0) throw new StartAfterEndDateException(startDate, endDate);
+        if (dayCount <= 0) throw new StartAfterEndDateException(startDate, endDate);
 
-        return days * averageMonthSalary;
+        return getVacationSalary(averageMonthSalary, dayCount);
     }
 
 
-    @GetMapping(value = CALC_VACATION_SALARY, params = {"average_month_salary", "day_count"})
-    public Double calcVacationSalary(
+    @GetMapping(value = GET_VACATION_SALARY, params = {"average_month_salary", "day_count"})
+    public Double getVacationSalary(
             @RequestParam(value = "average_month_salary") double averageMonthSalary,
-            @RequestParam(value = "day_count") Integer dayCount) {
+            @RequestParam(value = "day_count") long dayCount) {
 
+        return calcVacationSalary(averageMonthSalary, dayCount);
+    }
+
+    public static double calcVacationSalary(double averageMonthSalary, long dayCount){
         if (dayCount <= 0) throw new BadRequestException(
                 String.format("day_count must be positive, but given \"%s\"",
                         dayCount
                 ));
 
-        return averageMonthSalary * dayCount;
+        if (averageMonthSalary <= 0d) throw new BadRequestException(
+                String.format("average_month_salary must be positive, but given \"%s\"",
+                        averageMonthSalary
+                ));
+
+        return averageMonthSalary * dayCount / 29.3d;
     }
+
 }
